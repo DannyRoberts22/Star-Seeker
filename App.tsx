2c7049946447502f07363a12c64548c934484cc8
Danny Roberts <dannyroberts.sky@gmail.com>
2025-10-05 15:30:19 +0100

    Update splash and add lottie animation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AnimatedSplash from "./src/components/AnimatedSplash";
+import { useState } from "react";
 
 export default function App() {
+    const [showSplash, setShowSplash] = useState(true);
+
+  if (showSplash) return <AnimatedSplash onFinish={() => setShowSplash(false)} />;
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
